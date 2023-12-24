20bd4e964b29c686d386424a5604a6bdba0c041f
Anand Rajaram <anandrajaram21@gmail.com>
2023-12-24 19:10:38 +0530

    feat: add my new website

---
+++ src/middleware.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export default async function middleware(request: NextRequest) {
+  const host = request.nextUrl.host;
+
+  console.log("hostname", host);
+
+  if (host === "volt.place") {
+    return NextResponse.redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next
+     * - static (static files)
+     * - favicon.ico (favicon file)
+     * - public folder
+     * - public folder
+     */
+    "/((?!static|.*\\..*|_next|favicon.ico).*)",
+    "/",
+  ],
+};
