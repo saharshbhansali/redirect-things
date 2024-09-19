14689c566efd127d09884e0f387334d7511a8726
Saharsh Bhansali <75335566+saharshbhansali@users.noreply.github.com>
2024-09-19 13:10:03 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,13 +4,27 @@ import type { NextRequest } from "next/server";
 export default async function middleware(request: NextRequest) {
   const host = request.nextUrl.host;
 
+  const response = new NextResponse(`<html>
+  <head>
+    <title>Redirecting...</title>
+    <link rel="canonical" href="https://saharshbhansali.github.io/about-me/resume.pdf" />
+    <meta charset="utf-8" />
+    <meta http-equiv="refresh" content="0; url=https://saharshbhansali.github.io/about-me/resume.pdf" />
+  </head>
+  <body>
+    <p>Redirecting...</p>
+  </body>
+</html>
+`);
+
+  response.headers.set('Content-Type', 'text/html; charset=utf-8');
+
   console.log("hostname", host);
 
-  if (host === "www.volt.place") {
-    return NextResponse.redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+  if (host === "resume.saharshbhansali.dev") {
+    return response;
   }
 
-  return NextResponse.next();
 }
 
 export const config = {
